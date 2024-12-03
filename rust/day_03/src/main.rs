7225ceee8fff20cde6e3e066aebb82f002debe2e
Rickard Nyman <4slovb@gmail.com>
2024-12-04 00:10:35 +0100

    cleaned up

--- rust/day_03/src/main.rs
+++ rust/day_03/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use regex::Regex;
 
-fn solve(text:String) -> i32 {
+fn sum_products(text:String) -> i32 {
     let mut output = 0;
     let re_mul = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
     let mut results: Vec<(i32, i32)> = vec![];
@@ -12,21 +12,6 @@ fn solve(text:String) -> i32 {
         let (l, r) = res;
         output += l * r;
     }
-    // for row in rows {
-    //     let r = row.unwrap();
-    //     if is_safe(r.iter()) {
-    //         output += 1;
-    //     }
-    //     else {
-    //         for i in 0..r.len() {
-    //             // try without the number at index i
-    //             if is_safe(r.iter().enumerate().filter(|&(j, _)| j != i).map(|(_, e) | e)) {
-    //                 output += 1;
-    //                 break;
-    //             }
-    //         }
-    //     }
-    // }
     return output;
 }
 
@@ -34,6 +19,6 @@ fn main() {
     let file_name = "input.txt";
     // let file_name = "test.txt";
     let contents = fs::read_to_string(file_name).expect("Should have been able to read the file");
-    let answer = solve(contents);
+    let answer = sum_products(contents);
     println!("{answer}");
 }
