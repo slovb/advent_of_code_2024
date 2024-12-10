e661beafc964e42367c7d4ed8777a14d5e9e4d88
Rickard Nyman <4slovb@gmail.com>
2024-12-10 13:08:30 +0100

    Day 02, Java
    My academic Java is less readable than I would like.

---
+++ java/AoC2024/src/Day02.java
@@ -0,0 +1,131 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+
+public class Day02 {
+    private final String pathName;
+    private final List<int[]> rows = new ArrayList<>();
+
+    public Day02(String pathName) {
+        this.pathName = pathName;
+    }
+
+    private Day02 read() {
+        try {
+            File file = new File(this.pathName);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine().trim();
+                String[] parts = line.split(" ");
+
+                int[] row = new int[parts.length];
+                for (int i = 0; i < parts.length; i++) {
+                    row[i] = Integer.parseInt(parts[i]);
+                }
+                this.rows.add(row);
+            }
+            scanner.close();
+        }
+        catch (FileNotFoundException ignored) {
+            throw new Error(String.format("File not found %s", this.pathName));
+        }
+        return this;
+    }
+
+    private static boolean is_safe(int[] row, int skip) {
+        int last_diff = 0;
+        int i = 0;
+        if (skip == i) {
+            i += 1;
+        }
+        int prev = row[i];
+        for (i = i + 1; i < row.length; i++) {
+            if (skip == i) {
+                continue;
+            }
+            int diff = prev - row[i];
+            if (diff == 0 || Math.abs(diff) > 3) {
+                return false;
+            }
+            if (diff * last_diff < 0) { // swap signs
+                return false;
+            }
+            last_diff = diff;
+            prev = row[i];
+        }
+        return true;
+    }
+    private static boolean is_safe(int[] row) {
+        // -1 won't be an index so this is without skip, but a bit messy
+        return is_safe(row, - 1);
+    }
+
+    private int first() {
+        int output = 0;
+        for (int[] row : this.rows) {
+            if (is_safe(row)) {
+                output += 1;
+            }
+        }
+        return output;
+    }
+
+    private int second() {
+        int output = 0;
+        for (int[] row : this.rows) {
+            if (is_safe(row)) {
+                output += 1;
+            }
+            else {
+                for (int i = 0; i < row.length; i++) {
+                    if (is_safe(row, i)) {
+                        output += 1;
+                        break;
+                    }
+                }
+            }
+        }
+        return output;
+    }
+
+    // TEST CODE BELOW
+
+    public static int solveFirst(String pathName) {
+        return new Day02(pathName).read().first();
+    }
+
+    public static int solveSecond(String pathName) {
+        return new Day02(pathName).read().second();
+    }
+
+    public static void main(String[] args) {
+        Map<String, Integer> test_first = new HashMap<>();
+        Map<String, Integer> test_second = new HashMap<>();
+
+        String folder = "day_02";
+        String mainPathName = String.format("data/%s/input.txt", folder);
+        test_first.put(String.format("data/%s/test_0.txt", folder), 2);
+        test_second.put(String.format("data/%s/test_0.txt", folder), 4);
+
+        for (Map.Entry<String, Integer> entry : test_first.entrySet()) {
+            int solution = solveFirst(entry.getKey());
+            System.out.printf("First %s: %s\n", entry.getKey(), solution);
+            if (solution != entry.getValue()) {
+                System.out.println("Failed test");
+                return;
+            }
+        }
+        System.out.printf("First %s: %s\n\n", mainPathName, solveFirst(mainPathName));
+
+        for (Map.Entry<String, Integer> entry : test_second.entrySet()) {
+            int solution = solveSecond(entry.getKey());
+            System.out.printf("Second %s: %s\n", entry.getKey(), solution);
+            if (solution != entry.getValue()) {
+                System.out.println("Failed test");
+                return;
+            }
+        }
+        System.out.printf("Second %s: %s\n", mainPathName, solveSecond(mainPathName));
+    }
+}
