5479f5b44cd0969d608ac3f45c60b27300c62c3d
Rickard Nyman <4slovb@gmail.com>
2024-12-13 21:08:36 +0100

    day 13, z3 ended up being overkill, expected bad p2

---
+++ python/13/second.py
@@ -0,0 +1,105 @@
+from dataclasses import dataclass
+
+from z3 import Int, Solver
+
+
+@dataclass(frozen=True)
+class Machine:
+    button_a: tuple[int, int]
+    button_b: tuple[int, int]
+    prize: tuple[int, int]
+
+    def cost(self):
+        a = Int("a")
+        b = Int("b")
+
+        s = Solver()
+        s.add(a >= 0)
+        # s.add(a <= 100)
+        s.add(b >= 0)
+        # s.add(b <= 100)
+
+        s.add(self.prize[0] == a * self.button_a[0] + b * self.button_b[0])
+        s.add(self.prize[1] == a * self.button_a[1] + b * self.button_b[1])
+        res = s.check()
+        if res.r != 1:
+            return 0
+
+        # optimize the result
+        while res.r == 1:
+            model = s.model()
+            # print(model)
+            best_a = model[a].as_long()
+            best_b = model[b].as_long()
+            best = 3 * best_a + best_b
+            s.push()
+            s.add(3 * a + b < best)
+            res = s.check()
+            s.pop()
+        # print(best)
+        return best
+
+
+@dataclass(frozen=True)
+class Data:
+    input: list[Machine]
+
+
+def solve(data: Data) -> int:
+    out = 0
+    for machine in data.input:
+        # print(machine)
+        out += machine.cost()
+    return out
+
+
+def read(filename) -> Data:
+    with open(filename, "r") as f:
+        rows = [row.rstrip() for row in f.readlines()]
+        input = []
+        for row in rows:
+            if row.startswith("Button A:"):
+                _, l, r = row.split("+")
+                l = l.split(",")[0]
+                button_a = (int(l), int(r))
+            elif row.startswith("Button B:"):
+                _, l, r = row.split("+")
+                l = l.split(",")[0]
+                button_b = (int(l), int(r))
+            elif row.startswith("Prize:"):
+                _, l, r = row.split("=")
+                l = l.split(",")[0]
+                bump = 10000000000000
+                prize = (bump + int(l), bump + int(r))
+            else:
+                input.append(Machine(button_a=button_a, button_b=button_b, prize=prize))
+        input.append(Machine(button_a=button_a, button_b=button_b, prize=prize))
+        data = Data(input=input)
+        return data
+
+
+def main(filename):
+    return solve(read(filename))
+
+
+if __name__ == "__main__":
+    import sys
+
+    testcases = [
+        ("test_0.txt", 875318608908),
+    ]
+
+    if len(sys.argv) < 2:
+        has_failed = False
+        for filename, value in testcases:
+            res = main(filename)
+            print("{}   {}\n".format(filename, str(res)))
+            if res != value:
+                print("Failed test")
+                has_failed = True
+        if not has_failed:
+            filename = "input.txt"
+            print("{}   {}\n".format(filename, main(filename)))
+    else:
+        for f in sys.argv[1:]:
+            print("{}:\n{}\n".format(f, main(f)))
